1a365b1d82f5096727a088a28cb3f5d39606920b
Ana Sofia <sofiamelod@gmail.com>
2023-09-13 18:31:27 -0300

    feat: create Character repository

---
+++ src/main/java/com/rpg/ad/d/repository/CharacterRepository.java
@@ -0,0 +1,9 @@
+package com.rpg.ad.d.repository;
+
+import com.rpg.ad.d.entities.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Integer> {
+}
