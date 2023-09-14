852cf953d15df86359347c761378e94579c7cb35
Ana Sofia <sofiamelod@gmail.com>
2023-09-14 20:42:53 -0300

    feat: create findRandomMonster method in CharacterRepository

--- src/main/java/com/rpg/ad/d/repository/CharacterRepository.java
+++ src/main/java/com/rpg/ad/d/repository/CharacterRepository.java
@@ -2,8 +2,11 @@ package com.rpg.ad.d.repository;
 
 import com.rpg.ad.d.entities.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<Character, Integer> {
+    @Query(value = "SELECT * FROM character WHERE type = 'monstro' ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    Character findRandomMonster();
 }
