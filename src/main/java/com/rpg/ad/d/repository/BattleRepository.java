341ffa2a0a9bff21f0cfcceb58e22acc1218e01c
Ana Sofia <sofiamelod@gmail.com>
2023-09-14 19:03:17 -0300

    feat: add getBattleHistory method to BattleRepository

--- src/main/java/com/rpg/ad/d/repository/BattleRepository.java
+++ src/main/java/com/rpg/ad/d/repository/BattleRepository.java
@@ -2,8 +2,17 @@ package com.rpg.ad.d.repository;
 
 import com.rpg.ad.d.entities.Battle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BattleRepository extends JpaRepository<Battle, Integer> {
+    @Query(value = "SELECT battle.id, battle.user_character_id, battle.opponent_character_id, battle.winner, json_agg(turn.*) AS turns_data " +
+            "FROM battle " +
+            "LEFT JOIN turn ON battle.id = turn.battle_id " +
+            "WHERE battle.id = :battleId " +
+            "GROUP BY battle.user_character_id, battle.opponent_character_id", nativeQuery = true)
+    List<Object[]> getBattleHistory(int battleId);
 }
